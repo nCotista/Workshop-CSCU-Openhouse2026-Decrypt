848c4265ff1c31f6bcb6ef65bfd6159f85e36de7
nCotista <nCotista@users.noreply.github.com>
2026-03-26 16:41:56 +0000

    deploy: 3e19816acf11a2ec35db17c0d895de3994d522a3

---
+++ env.js
@@ -0,0 +1,5 @@
+window.ENV = {
+  API_KEY: 'AIzaSyBiiCRKoi1YBCbDSrmKmSIwXvCWVLRLzVY',
+  AUTH_DOMAIN: 'workshop-cscu-oph-2026.firebaseapp.com',
+  DB_URL: 'https://workshop-cscu-oph-2026-default-rtdb.asia-southeast1.firebasedatabase.app'
+};
